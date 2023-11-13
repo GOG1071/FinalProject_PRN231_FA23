dd1947868fb2e2613a0c645c2150d029390c5d1d
TungGPOCG <goldendarkness1071@gmail.com>
2023-11-13 19:45:52 +0700

    Base Controller

---
+++ WebApi/Controllers/Base/BaseController.cs
@@ -0,0 +1,43 @@
+﻿namespace WebApi.Controllers.Base;
+
+using BusinessObject.Models;
+using DataAccess.Repository.Base;
+using Microsoft.AspNetCore.Mvc;
+
+public abstract class BaseController<TModel,TRepo> : ControllerBase, IBaseController<TModel> where TModel : class, IModel where TRepo : IRepository<TModel> 
+{
+    protected TRepo repo;
+    
+    [HttpGet]
+    public virtual ActionResult<IEnumerable<TModel>> GetAll()
+    {
+        return this.repo.GetAll();
+    }
+    public virtual ActionResult<TModel> Add(TModel t)
+    {
+        var model = this.repo.Get(t.PrimaryKey);
+        if (model != null) { return Conflict(); }
+        this.repo.Add(t);
+        return this.NoContent();
+    }
+    public virtual ActionResult<TModel> Update(TModel t)
+    {
+        var model = this.repo.Get(t.PrimaryKey);
+        if (model == null) { return NotFound(); }
+        this.repo.Update(t);
+        return this.NoContent();
+    }
+    public virtual ActionResult<TModel> DeleteAbstract(object[]? objects)
+    {
+        var model = this.repo.Get(objects);
+        if (model == null) { return NotFound(); }
+        this.repo.Delete(objects);
+        return this.NoContent();
+    }
+    public virtual ActionResult<TModel> GetAbstract(object[]? objects)
+    {
+        var model = this.repo.Get(objects);
+        if (model == null) { return NotFound(); }
+        return model;
+    }
+}
---
+++ WebApi/Controllers/Base/IBaseController.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Controllers.Base;
+
+using BusinessObject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public interface IBaseController<T> where T : class, IModel
+{
+    ActionResult<IEnumerable<T>> GetAll();
+    ActionResult<T>              Add(T t);
+    ActionResult<T>              Update(T t);
+    
+    ActionResult<T> DeleteAbstract(object[]? objects);
+    ActionResult<T> GetAbstract(object[]? objects);
+}
