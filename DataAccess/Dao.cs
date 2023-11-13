dc80ba21ec6af7ad5f8960feedfd66e6f5885f90
TungGPOCG <goldendarkness1071@gmail.com>
2023-11-13 17:05:32 +0700

    Base DAO

---
+++ DataAccess/Dao.cs
@@ -0,0 +1,78 @@
+﻿namespace DataAccess;
+
+using BusinessObject.Models;
+
+public abstract class Dao<T> : IDao<T> where T : class, IModel
+{
+    public static T Get(object[]? objects)
+    {
+        try
+        {
+            using var context = new Prn231DBContext();
+            var list = context.Set<T>().ToList();
+            return list.FirstOrDefault(x => x.PrimaryKey != null && x.PrimaryKey.SequenceEqual(objects));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null;
+        }
+    }
+    public static T       Get(T t)                  { throw new NotImplementedException(); }
+    public static List<T> GetAll()
+    {
+        try
+        {
+            using var context = new Prn231DBContext();
+            return context.Set<T>().ToList();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null;
+        }
+    }
+    public static void Add(T t)
+    {
+        try
+        {
+            using var context = new Prn231DBContext();
+            context.Set<T>().Add(t);
+            context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    public static void Update(T t)
+    {
+        try
+        {
+            using var context = new Prn231DBContext();
+            context.Set<T>().Update(t);
+            context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    public static void Delete(object[]? objects)
+    {
+        try
+        {
+            using var context = new Prn231DBContext();
+            var t = Get(objects);
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
---
+++ DataAccess/IDao.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccess;
+
+using BusinessObject.Models;
+
+public interface IDao<T> where T : class, IModel
+{
+    static abstract T Get(object[]? objects);
+    static abstract T Get(T t);
+    static abstract List<T> GetAll();
+    static abstract void Add(T t);
+    static abstract void Update(T t);
+    static abstract void Delete(object[]? objects);
+}
